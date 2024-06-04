5037d59ade828f2216478bd3c26cf0cd53d943cf
Arturo-MT <ingatorrez@gmail.com>
2024-06-04 17:46:43 -0600

    feat: Add App component with LoginForm

---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../containers/login-form'
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
